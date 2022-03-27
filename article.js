078464b12896ffe9f7ff8187448e6b2c83386033
kadekframa <dframa19@gmail.com>
2022-03-27 15:11:18 +0800

    Web Component. Menangani data kompleks. Semangat Berlatih !!!

---
+++ article.js
@@ -0,0 +1,8 @@
+const article = {
+    id: 1,
+    title: "Lorem Ipsum",
+    featuredImage: "https://picsum.photos/id/204/536/354?grayscal",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry"
+}
+
+export default article;
---
+++ script.js
@@ -0,0 +1,11 @@
+// Menangani data kompleks.
+
+import "./article-item.js"
+import article from "./article.js";
+
+const containerElement = document.querySelector(".container");
+
+const articleItemElement = document.createElement("article-item");
+articleItemElement.article = article;
+
+containerElement.appendChild(articleItemElement);
