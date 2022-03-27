2ece72b61d249d27a15b4c8cf72865c2a6823435
kadekframa <dframa19@gmail.com>
2022-03-27 11:14:51 +0800

    Web Component. Atribut dan method pada Custom Element. Semangat Berlatih !!!

--- image-figure.js
+++ image-figure.js
@@ -1,2 +1,20 @@
 // Web Component JS.
 
+class ImageFigure extends HTMLElement{
+    connectedCallback(){
+        this.src  = this.getAttribute("src") || null;
+        this.alt  = this.getAttribute("alt") || null;
+        this.caption  = this.getAttribute("caption") || null;
+
+        this.innerHTML = `
+            <figure>
+                <img src="${this.src}"
+                    alt="${this.alt}">
+                <figcaption>${this.caption}</figcaption>
+                </img>
+            </figure>
+        `;
+    }
+}
+
+customElements.define("image-figure", ImageFigure);
